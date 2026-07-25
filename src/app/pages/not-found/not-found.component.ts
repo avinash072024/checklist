9066a194c0e6b32f647b911e8c68e120606a41f2
avinash072024 <www.avi.marbhal@gmail.com>
2026-07-26 00:03:11 +0530

    page not found implemented

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {
+ location = inject(Location);
 
+ back(): void {
+  this.location.back();
+ }
 }
