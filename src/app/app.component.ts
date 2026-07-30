e35a5468e6ab021f72b773e0f479c65c1d4b61c9
avinash072024 <www.avi.marbhal@gmail.com>
2026-07-30 16:27:36 +0530

    PWA banner removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 import * as AOS from 'aos';
 import { NetworkService } from './services/network/network.service';
 import { OfflinePageComponent } from './pages/offline-page/offline-page.component';
-import { PwaInstallBannerComponent } from './components/pwa-install-banner/pwa-install-banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgxSpinnerModule, OfflinePageComponent, PwaInstallBannerComponent],
+  imports: [RouterOutlet, NgxSpinnerModule, OfflinePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
