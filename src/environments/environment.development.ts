35d0fbb5029023d87e7b90e3a9e131ded3e644d5
avinash072024 <www.avi.marbhal@gmail.com>
2026-07-30 23:13:01 +0530

    environment socketUrl added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
-    // apiUrl: 'http://localhost:5000/api',
+    production: false,
+    // apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
+    apiUrl: 'http://localhost:5000/api',
+    // socketUrl: 'http://localhost:5000',
+    socketUrl: 'https://checklist-backend-nqq6.onrender.com',
 };
