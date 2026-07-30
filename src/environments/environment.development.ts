e4bdc6ba75554017b8715588c89a973f6b262314
avinash072024 <www.avi.marbhal@gmail.com>
2026-07-30 23:24:17 +0530

    url changed in ennvironment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
-    apiUrl: 'http://localhost:5000/api',
+    apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
+    // apiUrl: 'http://localhost:5000/api',
     // socketUrl: 'http://localhost:5000',
     socketUrl: 'https://checklist-backend-nqq6.onrender.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
     // apiUrl: 'http://localhost:5000/api',
+    // socketUrl: 'http://localhost:5000',
+    socketUrl: 'https://checklist-backend-nqq6.onrender.com',
 };
