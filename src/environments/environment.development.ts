c7a4a5d53f09a3fe6b987f0f7918943c72e35abe
avinash072024 <www.avi.marbhal@gmail.com>
2026-07-31 11:30:56 +0530

    backend URL changed bcoz backend deployed on other server

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,17 @@
+// export const environment = {
+//     production: true,
+//     apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
+//     // apiUrl: 'http://localhost:5000/api',
+//     // socketUrl: 'http://localhost:5000',
+//     socketUrl: 'https://checklist-backend-nqq6.onrender.com',
+// };
+
+
+
 export const environment = {
-    production: false,
-    apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
+    production: true,
+    apiUrl: 'https://checklist-backend-two.vercel.app/api',
     // apiUrl: 'http://localhost:5000/api',
     // socketUrl: 'http://localhost:5000',
-    socketUrl: 'https://checklist-backend-nqq6.onrender.com',
+    socketUrl: 'https://checklist-backend-two.vercel.app',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,16 @@
+// export const environment = {
+//     production: false,
+//     apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
+//     // apiUrl: 'http://localhost:5000/api',
+//     // socketUrl: 'http://localhost:5000',
+//     socketUrl: 'https://checklist-backend-nqq6.onrender.com',
+// };
+
+
 export const environment = {
     production: false,
-    apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
+    apiUrl: 'https://checklist-backend-two.vercel.app/api',
     // apiUrl: 'http://localhost:5000/api',
     // socketUrl: 'http://localhost:5000',
-    socketUrl: 'https://checklist-backend-nqq6.onrender.com',
+    socketUrl: 'https://checklist-backend-two.vercel.app',
 };
