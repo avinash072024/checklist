a984b8d7086193922272f79d19c21b713c9605e3
avinash072024 <www.avi.marbhal@gmail.com>
2026-09-06 12:09:41 +0530

    again changes for token

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -2,5 +2,5 @@ export class Constants {
     public static appName: string = 'CheckList'
     public static token: string = `${this.appName.toLowerCase()}-token`;
     public static listDetails: string = `${this.appName.toLowerCase()}-listDetails`;
-    public static THEME_KEY: string = (this.appName.toLowerCase() + 'app-theme').toLowerCase();
+    public static THEME_KEY: string = (this.appName.toLowerCase() + '-app-theme').toLowerCase();
 }
