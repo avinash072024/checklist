8ba06ff176f926c66c7a41a86587e6636b8b5c76
avinash072024 <www.avi.marbhal@gmail.com>
2026-09-18 22:45:11 +0530

    API Url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,7 @@
-// export const environment = {
-//     production: false,
-//     apiUrl: 'https://checklist-backend-nqq6.onrender.com/api',
-//     // apiUrl: 'http://localhost:5000/api',
-//     // socketUrl: 'http://localhost:5000',
-//     socketUrl: 'https://checklist-backend-nqq6.onrender.com',
-// };
-
-
 export const environment = {
     production: false,
-    apiUrl: 'https://checklist-backend-two.vercel.app/api',
+    apiUrl: 'https://checklist-backend-livid.vercel.app/api',
     // apiUrl: 'http://localhost:5000/api',
     // socketUrl: 'http://localhost:5000',
-    socketUrl: 'https://checklist-backend-two.vercel.app',
+    socketUrl: 'https://checklist-backend-livid.vercel.app',
 };
