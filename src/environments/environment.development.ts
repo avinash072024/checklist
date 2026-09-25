51b42e6a3f9b1dcb249ed9a526659397d709973a
avinash072024 <www.avi.marbhal@gmail.com>
2026-09-25 23:43:53 +0530

    some essential changes done

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://checklist-backend-delta.vercel.app/api',
+    apiUrl: 'https://checklist-backend-xi.vercel.app/api',
     // apiUrl: 'http://localhost:5000/api',
     // socketUrl: 'http://localhost:5000',
-    socketUrl: 'https://checklist-backend-delta.vercel.app',
+    socketUrl: 'https://checklist-backend-xi.vercel.app',
 };
